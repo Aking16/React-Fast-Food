6f0e9126cf5b96b5f4f098c6c49c6fae759f25c7
Aking <www.aking016@gmail.com>
2023-08-13 22:22:44 +0330

    Released Version 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/React-Fast-Food/",
+  base: "/React-Fast-Food/",
   plugins: [react()],
 });
